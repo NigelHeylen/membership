0db57dabeb2e8610cc12626abd49ea0d10c393fc
Nigel Heylen <nigel@webcomrades.be>
2018-12-12 15:53:25 +0100

    rs -> wc

--- ci/push.sh
+++ ci/push.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-ENVIRONMENT=$1
-clj -A:dev -m datomic.ion.dev '{:op :push :creds-profile "rs'$ENVIRONMENT'" :region "eu-central-1" :uname "'`git rev-parse HEAD`'"}'
+clj -A:dev -m datomic.ion.dev '{:op :push :creds-profile wc :region "eu-central-1" :uname "'`git rev-parse HEAD`'"}'
