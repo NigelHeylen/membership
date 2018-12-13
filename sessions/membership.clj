2a47fb006f62a65f74de72f0aeffa46a99e7ecdc
Nigel Heylen <nigel@webcomrades.be>
2018-12-13 15:21:28 +0100

    Add transit

--- sessions/membership.clj
+++ sessions/membership.clj
@@ -1,6 +1,8 @@
 (ns membership
   (:require [datomic.ion.starter :as starter]
-            [datomic.client.api :as d]))
+            [datomic.client.api :as d]
+            [cognitect.transit :as transit])
+  (:import (java.io ByteArrayOutputStream ByteArrayInputStream)))
 
 
 (def conn (starter/get-connection))
@@ -12,3 +14,17 @@
 
 (comment
   (starter/start-training {:input "\"training.type/gi\""}))
+
+(def out (ByteArrayOutputStream. 4096))
+(def writer (transit/writer out :json))
+
+
+(transit/write writer {:id (java.util.UUID/randomUUID)})
+
+(.toString out)
+
+
+(def in (ByteArrayInputStream. (.toByteArray out)))
+(def reader (transit/reader in :json))
+
+(type (:id (transit/read reader)))
